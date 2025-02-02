cb7c7a81ba583467e1c04742b42363feda50583d
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-02 20:37:34 +0100

    fixing cross origin issues

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Fortune - The Brand." />
+        <meta charSet="UTF-8" />
+
+        <meta name="description" content="Fortune -The Brand." />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
       </Head>
       <body className="font-inter bg-gray-100 text-gray-900">
         <Main />
@@ -20,3 +16,26 @@ export default function Document() {
     </Html>
   );
 }
+
+// import { Html, Head, Main, NextScript } from "next/document";
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         <meta name="description" content="Fortune - The Brand." />
+//         <link rel="icon" href="/favicon.ico" />
+//         <link rel="preconnect" href="https://fonts.googleapis.com" />
+//         <link
+//           rel="preconnect"
+//           href="https://fonts.gstatic.com"
+//           crossOrigin="anonymous"
+//         />
+//       </Head>
+//       <body className="font-inter bg-gray-100 text-gray-900">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
