9854386728ca37eb00eb96ff0047f62b4361d61f
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-02 20:41:27 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        has: [{ type: "host", value: "fortunecaribido.vercel.app" }],
-        destination: "https://fortunecaribido.vercel.app",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         has: [{ type: "host", value: "fortunecaribido.vercel.app" }],
+//         destination: "https://fortunecaribido.vercel.app",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
