193f973426abde6a7c457e1d73fabe14a3e50e30
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-03 18:46:53 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Head>
-        <title>Fortune Chinenyem-Aribido</title>
-        <meta
-          name="description"
-          content="Fortune is a Software Developer, Tutor, Coach"
-        />
-      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
