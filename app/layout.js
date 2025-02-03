2c231e9db4e44df69e1416df1ea145cdfaff0e21
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-03 18:38:55 +0100

    update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Fortune Chinenyem-Aribido</title>
+        <meta
+          name="description"
+          content="Fortune is a Software Developer, Tutor, Coach"
+        />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
